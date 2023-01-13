4c818e5f66011098f69d39ace6b730be10f39288
Exaxis <mwinstead49@gmail.com>
2023-01-13 14:36:11 -0500

    - Initial commit with Angular packaged as a sub-folder and basic electron code

---
+++ src/main/main.ts
@@ -0,0 +1,46 @@
+import { app, BrowserWindow, ipcMain, Menu } from 'electron';
+import * as path from 'path';
+import * as os from 'os';
+
+let win: BrowserWindow | null = null;
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // Disabled Node integration
+      nodeIntegration: false,
+      // protect against prototype pollution
+      contextIsolation: true,
+      // turn off remote
+      //enableRemoteModule: false,
+      // Preload script
+      preload: path.join(app.getAppPath(), 'dist/preload', 'preload.js')
+    }
+  });
+
+  // https://stackoverflow.com/a/58548866/600559
+  Menu.setApplicationMenu(null);
+
+  win.loadFile(path.join(app.getAppPath(), 'dist/renderer', 'index.html'));
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+ipcMain.on('dev-tools', () => {
+  if (win) {
+    win.webContents.toggleDevTools();
+  }
+});
+
